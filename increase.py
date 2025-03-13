8ea44a491e21aec5a10a8a1d639ffae7adea64a5
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-03-13 23:30:17 +0300

    fixed ps

--- increase.py
+++ increase.py
@@ -7,7 +7,7 @@ import os
 class Increase :
     def __init__(self,Progress_Bar):
         self.progress_bar = Progress_Bar
-
+        
     def increase(self,Ratio):
         """
         Updates the progress bar based on the strength of the password.
