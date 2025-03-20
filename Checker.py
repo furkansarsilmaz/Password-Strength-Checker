22e7499302624213ca61d9ad10b57ab6ce4c6ccc
furkansarsilmaz <160139624+furkansarsilmaz@users.noreply.github.com>
2025-03-20 23:03:15 +0300

    solid

--- Checker.py
+++ Checker.py
@@ -1,42 +1,11 @@
-from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
-from increase import Increase
-from saver import Saver
 import re
-import os
+from tkinter import messagebox
+class Check :
+    def __init__(self,saver,increaser):
+        self.saver = saver
+        self.Increase = increaser
 
-class Checker():
-    def __init__(self, root):
-        """
-        Initializes the UI components using Tkinter, 
-        setting up labels, entry fields, buttons, and a progress bar.
-        """
-        self.root = root
-        self.root.geometry("300x200")
-        self.root.title("Checker")
-
-        self.Menu_Label = Label(self.root, text="Enter a Password", font=("Arial", 14, "bold italic")).pack(pady=10)
-
-        self.Password_Text = Entry(self.root, show="*")
-        self.Password_Text.pack()
-
-        self.Progress_Bar = ttk.Progressbar(self.root, mode="determinate")
-        self.Progress_Bar.pack(pady=10)
-
-        self.Button_Frame = Frame(self.root, relief=RAISED)
-        self.Button_Frame.pack()
-
-        self.Increase = Increase(self.Progress_Bar)
-        self.Enter_Button = Button(self.Button_Frame, text="Enter", width=5, height=2, command= self.Check_Password)
-        self.Enter_Button.pack(pady=10, side=LEFT)
-
-        self.Quit_Button = Button(self.Button_Frame, text="Quit", width=5, height=2, command=self.root.quit)
-        self.Quit_Button.pack(side=RIGHT)
-
-        self.saver = Saver()
-
-    def Check_Password(self):
+    def Check_Password(self,text):
         """
         Validates the entered password against three regex patterns 
         (strong, medium, weak). 
@@ -45,7 +14,8 @@ class Checker():
         Password_Regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$'
         Weak_Regex = r'^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]{6,}'
         Middle_Regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$'
-        Password = self.Password_Text.get()
+        self.text = text
+        Password = self.text.get()
 
         if re.match(Password_Regex, Password):
             messagebox.showinfo("Succeed", "Password is strong")
@@ -59,8 +29,3 @@ class Checker():
         elif re.match(Middle_Regex, Password):
             messagebox.showwarning("Middle", "Password is mid-level, try again")
             self.Increase.increase(66)
-
-if __name__ == "__main__":
-    root = Tk()
-    Checker(root)
-    root.mainloop()
---
+++ main.py
@@ -0,0 +1,65 @@
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+from increase import Increase
+from saver import Saver
+from checker import Check
+import re
+import os
+
+class Checker():
+    def __init__(self, root):
+        """
+        Initializes the UI components using Tkinter, 
+        setting up labels, entry fields, buttons, and a progress bar.
+        """
+        self.root = root
+        self.root.geometry("300x200")
+        self.root.title("Checker")
+
+
+        self.Menu_Label = Label(self.root, text="Enter a Password", font=("Arial", 14, "bold italic")).pack(pady=10)
+
+        self.Password_Text = Entry(self.root, show="*")
+        self.Password_Text.pack()
+
+        self.Progress_Bar = ttk.Progressbar(self.root, mode="determinate")
+        self.Progress_Bar.pack(pady=10)
+
+        self.Button_Frame = Frame(self.root, relief=RAISED)
+        self.Button_Frame.pack()
+
+        self.saver = Saver()
+        self.Increase = Increase(self.Progress_Bar)
+        self.check = Check(self.saver,self.Increase)
+        
+        self.Enter_Button = Button(self.Button_Frame, text="Enter", width=5, height=2, command= lambda: self.check.Check_Password(self.Password_Text))
+        self.Enter_Button.pack(pady=10, side=LEFT)
+
+        self.Quit_Button = Button(self.Button_Frame, text="Quit", width=5, height=2, command=self.root.quit)
+        self.Quit_Button.pack(side=RIGHT)
+
+    """
+    def Check_Password(self):
+        Password_Regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,16}$'
+        Weak_Regex = r'^(?=.*[a-z])(?=.*[A-Z])[A-Za-z]{6,}'
+        Middle_Regex = r'^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)[A-Za-z\d]{8,}$'
+        Password = self.Password_Text.get()
+
+        if re.match(Password_Regex, Password):
+            messagebox.showinfo("Succeed", "Password is strong")
+            self.saver.Save_Password(Password)  
+            self.Increase.increase(99)
+
+        elif re.match(Weak_Regex, Password):
+            messagebox.showwarning("Weak", "Password is weak, try again")
+            self.Increase.increase(33)
+
+        elif re.match(Middle_Regex, Password):
+            messagebox.showwarning("Middle", "Password is mid-level, try again")
+            self.Increase.increase(66)
+    """
+if __name__ == "__main__":
+    root = Tk()
+    Checker(root)
+    root.mainloop()
